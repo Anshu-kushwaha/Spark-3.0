834e2c4a1af6e8475d760cc7ca7c86641da87f15
Anshu <anshukushwaha112@gmail.com>
2026-02-27 19:47:25 +0530

    auth added

--- Spark.java
+++ Spark.java
@@ -7,4 +7,12 @@ class Spark{
     public void Payment(){
         System.out.println("m1");
     }
+
+    public void notifyUser() {
+        System.out.println("Hello user");
+    }
+
+    public void Authentication(){
+        System.out.println("Auth");
+    }
 } 
